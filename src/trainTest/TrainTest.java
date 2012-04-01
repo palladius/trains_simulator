b724b38618355a0df4ff25936008308c69d97399
Riccardo Carlesso (Gic) <riccardo_carlesso@dell.com>
2012-04-01 16:52:10 +0100

    added tests 8they dont work)

---
+++ src/trainTest/TrainTest.java
@@ -0,0 +1,75 @@
+/**
+ * 
+ */
+package trainTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Riccardo_Carlesso
+ *
+ */
+public class TrainTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Train#Train(int, int, int, int)}.
+	 */
+	@Test
+	public final void testTrain() {
+		fail("Not yet implemented");
+		Train t = new Train(0,10,0,10);
+		//    public Train(int n, int railway_period, int initial_position, int cargo_capacity) {
+
+	}
+
+	/**
+	 * Test method for {@link Train#set_status(Train.TrainStatus)}.
+	 */
+	@Test
+	public final void testSet_status() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link Train#loadCargo()}.
+	 */
+	@Test
+	public final void testLoadCargo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
